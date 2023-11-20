da9e2c84115f07ffdf18318e04946bb8de24a4c9
newuzbdeveloper <106350805+newuzbdeveloper@users.noreply.github.com>
2023-11-20 16:11:47 +0100

    New dependencies of prettier.

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
+import App from "./app/App.tsx";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./app/App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
